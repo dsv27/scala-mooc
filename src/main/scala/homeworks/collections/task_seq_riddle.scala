a9cbddce40ced4a339de739993f04e0924da998f
Dmitry Vertlib <dsv@dsv27.ru>
2020-12-24 19:31:26 +0300

    Completing homework # 2

--- src/main/scala/homeworks/collections/task_seq_riddle.scala
+++ src/main/scala/homeworks/collections/task_seq_riddle.scala
@@ -16,21 +16,28 @@ object task_seq_riddle {
     * ...........
     *
     * 1. Реализуйте функцию генерирующую след последовательность из текущей
-    * For Quicky #10: Here are sets of numbers enclosed by square brackets. 
-    * Fill the next set of numbers. [1],[1,1],[2,1],[1,2,1,1],[1,1,1,2,2,1],[3,1,2,2,1,1], [.............] 
-    * Start with [1], we count 1 number 1, so we write [1, 1], then we continue counting 2 numbers 1, so we write [2, 1], now that's 1 number 2, and 1 number 1, so we write [1, 2, 1, 1],... 
-    * So a few next sets are: [1, 1, 1, 2, 2, 1] [3, 1, 2, 2, 1, 1] [1, 3, 1, 1, 2, 2, 2, 1] [1, 1, 1, 3, 2, 1, 3, 2, 1, 1] 
+    * For Quicky #10: Here are sets of numbers enclosed by square brackets.
+    * Fill the next set of numbers. [1],[1,1],[2,1],[1,2,1,1],[1,1,1,2,2,1],[3,1,2,2,1,1], [.............]
+    * Start with [1], we count 1 number 1, so we write [1, 1], then we continue counting 2 numbers 1, so we write [2, 1], now that's 1 number 2, and 1 number 1, so we write [1, 2, 1, 1],...
+    * So a few next sets are: [1, 1, 1, 2, 2, 1] [3, 1, 2, 2, 1, 1] [1, 3, 1, 1, 2, 2, 2, 1] [1, 1, 1, 3, 2, 1, 3, 2, 1, 1]
     * Source https://www.physicsforums.com/threads/brain-teasers-and-math-problems.117230/
     */
 
   def nextLine(currentLine: List[Int]): List[Int] = {
-   @tailrec
+    @tailrec
     def loop(acc: List[Int], l: List[Int]): List[Int] = l match {
       case Nil => acc
-      case thisList@(head :: _) =>
-        val tmpList = thisList.takeWhile(_ == head)
-        val resultList = List(tmpList.size, head)
-        loop(acc ::: resultList, thisList.dropWhile(_ == head))
+      case thisList @ (head :: _) =>
+        val tmpList =
+          thisList.takeWhile(_ == head) //выбираем записи до последнего head
+        val resultList = List(
+          tmpList.size,
+          head
+        ) // создаем промежуточный результирующий список, из 2 элементов head = длина непрерывной последовательности, tail значение эдемента
+        loop(
+          acc ::: resultList,
+          thisList.dropWhile(_ == head)
+        ) // добавляем в глову аккумулятора результирующий список, передаем в обработку откушеный хвост
     }
 
     loop(Nil, currentLine)
